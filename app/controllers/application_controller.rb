0c4b9859e132d1ed44fe7d0fed76dc63d669a490
hokuma <okuma@hokuma.net>
2013-12-21 13:42:05 +0000

    Finish sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+
+  # for csrf protection
+  def handle_unverified_request
+    sign_out
+    supper
+  end
 end
